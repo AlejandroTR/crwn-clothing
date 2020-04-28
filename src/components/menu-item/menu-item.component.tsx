86a5101a0084518a0a8d2da5cbb230d7b7829132
Alex <smileyrec@gmail.com>
2020-04-28 15:46:47 +0200

    update our menu-items to navigate withRouter HOC

--- src/components/menu-item/menu-item.component.tsx
+++ src/components/menu-item/menu-item.component.tsx
@@ -1,21 +1,33 @@
 import React, { FunctionComponent } from 'react';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 
 import './menu-item.styles.scss'
 
-interface MenuItemProps {
+interface MenuItemProps extends RouteComponentProps<any> {
     title: string;
     imageUrl: string;
     size?: string
+    linkUrl: string;
 }
 
-const MenuItem: FunctionComponent<MenuItemProps> = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
-        <div className='background-image' style={{backgroundImage: `url(${imageUrl}`}} />
-        <div className='content'>
-            <h1 className='title'>{title}</h1>
-            <span className='subtitle'>SHOP NOW</span>
+const MenuItem: FunctionComponent<MenuItemProps> =
+    ({
+         title,
+         imageUrl,
+         size,
+         history,
+         linkUrl,
+         match
+    }) => (
+        <div
+            className={`${size} menu-item`}
+            onClick={() => history.push(`${match.url}${linkUrl}`)}>
+            <div className='background-image' style={{backgroundImage: `url(${imageUrl}`}} />
+            <div className='content'>
+                <h1 className='title'>{title}</h1>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
         </div>
-    </div>
-);
+    );
 
-export default MenuItem;
+export default withRouter(MenuItem);
